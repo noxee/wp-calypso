c5b39cb7b8f8a640a074fbe6a886a693334bd4b9
Dennis Snell <dennis.snell@automattic.com>
2016-04-07 11:11:18 +0100

    Add page view tracking to wpcom plugins page

--- client/my-sites/plugins-wpcom/plugin-panel.jsx
+++ client/my-sites/plugins-wpcom/plugin-panel.jsx
@@ -4,11 +4,13 @@ import InfoHeader from './info-header';
 import StandardPluginsPanel from './standard-plugins-panel';
 import PremiumPluginsPanel from './premium-plugins-panel';
 import BusinessPluginsPanel from './business-plugins-panel';
+import PageViewTracker from 'client/analytics/page-view-tracker';
 
 export const PluginPanel = React.createClass( {
 	render() {
 		return (
 			<div className="wpcom-plugin-panel">
+				<PageViewTracker path="/plugins/:site" title="Plugins > WPCOM Site" />
 				<InfoHeader />
 				<StandardPluginsPanel />
 				<PremiumPluginsPanel />
